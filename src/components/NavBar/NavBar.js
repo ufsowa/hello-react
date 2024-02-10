b26d529fc396850e915399dd35db94b916782889
Piotr Sowa <piotrsowa87@gmail.com>
2024-02-10 15:51:36 +0100

    Style navigation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import styles from './NavBar.module.scss'
+import{ Link, NavLink} from 'react-router-dom'
 
 const NavBar = props => {
     return (
         <nav className={styles.nav}>
-            <a href="/"><i className="fa fa-tasks"></i></a>
+            <Link to="/"><i className="fa fa-tasks"></i></Link>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/favorite">Favorite</a></li>
-                <li><a href="/about">About</a></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
             </ul>
         </nav>
     );
